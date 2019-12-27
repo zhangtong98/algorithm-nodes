2e4953deac5b357696b0c73cea7c5f0239f5dcbd
zhangtong <18223524689@163.com>
2019-12-27 17:30:52 +0800

    20191227pmV2

--- src/com/zhang/leetCode/easy/FirstUniqChar.java
+++ src/com/zhang/leetCode/easy/FirstUniqChar.java
@@ -12,7 +12,22 @@ package com.zhang.leetCode.easy;
 public class FirstUniqChar {
 
     public static void main(String[] args) {
-        System.out.println(firstUniqChar("aassxxss"));
+        System.out.println(firstUniqCharV2("loveleetcode"));
+    }
+
+    public static int firstUniqCharV2(String s) {
+        int index = -1;
+        //反过来，只有26个字符
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            int beginIndex = s.indexOf(ch);
+            // 从头开始的位置是否等于结束位置，相等说明只有一个，
+            if (beginIndex != -1 && beginIndex == s.lastIndexOf(ch)) {
+                //取小的，越小代表越前。
+                index = (index == -1 || index > beginIndex) ? beginIndex : index;
+
+            }
+        }
+        return index;
     }
 
     public static int firstUniqChar(String s) {
